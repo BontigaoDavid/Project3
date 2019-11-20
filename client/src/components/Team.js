af9a6eceac2d4e99f25b7bdcf0fd10652bff30f9
J Tsai <jtsai972@gmail.com>
2019-11-20 13:10:47 -0600

    formatted the image some

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,20 +1,36 @@
 import React from "react";
 
 function Website(props) {
-  let responses = []
-  if(props.website) {
+  let responses = [];
+  if (props.website) {
     console.log(props.website);
-    responses.push(<p><a href={props.website}>Website - Jtsai972.space</a></p>);
+    responses.push(
+      <p>
+        <a href={props.website}>Website - Jtsai972.space</a>
+      </p>
+    );
   }
 
-  if(props.github) {
+  if (props.github) {
     console.log(props.website);
-    responses.push(<p><a href={"https://github.com/" + props.github}>Github - {props.github}</a></p>);
+    responses.push(
+      <p>
+        <a href={"https://github.com/" + props.github}>
+          Github - {props.github}
+        </a>
+      </p>
+    );
   }
 
-  if(props.linkedin) {
+  if (props.linkedin) {
     console.log(props.website);
-    responses.push(<p><a href={"https://www.linkedin.com/in/" + props.linkedin}>LinkedIn - {props.linkedin}</a></p>);
+    responses.push(
+      <p>
+        <a href={"https://www.linkedin.com/in/" + props.linkedin}>
+          LinkedIn - {props.linkedin}
+        </a>
+      </p>
+    );
   }
 
   return responses;
@@ -26,14 +42,14 @@ function Team(props) {
   return (
     <div className="col-12 col-sm-4 col-xl-3">
       <div className="card">
-        <img
-          className="card-img-top"
-          src={props.src}
-          alt={props.name}
-        />
+        <img className="card-img-top" src={props.src} alt={props.name} />
         <div className="card-body">
           <h4 className="h5 card-title">{props.name}</h4>
-          <Website website={props.website} github={props.github} linkedin={props.linkedin} />
+          <Website
+            website={props.website}
+            github={props.github}
+            linkedin={props.linkedin}
+          />
         </div>
       </div>
     </div>
