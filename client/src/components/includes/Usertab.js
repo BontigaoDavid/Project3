2b55f76e754984fc6e1ef007ca6c0be1cce9c97a
Dohn Joe <johnphtdo@gmail.com>
2019-11-21 10:29:10 -0600

    Usertab component to clean up home page

---
+++ client/src/components/includes/Usertab.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Login from "../Login";
+import Signup from "../Signup";
+
+function Usertab() {
+  return (
+    <div className="col-12 col-md-6 col-lg-5">
+      <ul
+        className="nav nav-tabs nav-fill nav-justified"
+        id="myTab"
+        role="tablist"
+      >
+        <li className="nav-item">
+          <a
+            className="nav-link active"
+            id="signup-tab"
+            data-toggle="tab"
+            href="#signup"
+            role="tab"
+            aria-controls="signup"
+            aria-selected="true"
+          >
+            Sign Up
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="nav-link"
+            id="login-tab"
+            data-toggle="tab"
+            href="#login"
+            role="tab"
+            aria-controls="login"
+            aria-selected="false"
+          >
+            Login
+          </a>
+        </li>
+      </ul>
+
+      <div className="tab-content">
+        <div
+          className="tab-pane active"
+          id="signup"
+          role="tabpanel"
+          aria-labelledby="signup-tab"
+        >
+          <Signup />
+        </div>
+        <div
+          className="tab-pane"
+          id="login"
+          role="tabpanel"
+          aria-labelledby="login-tab"
+        >
+          <Login />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Usertab;
