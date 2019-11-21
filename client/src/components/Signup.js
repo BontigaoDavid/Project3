076d04388a752db072ca58ed079fcd40842033a7
David Bontigao <bontigaodavid@gmail.com>
2019-11-21 11:48:42 -0600

    logged in bcrypt compare

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import API from "../utils/API";
-import {Input, FormBtn} from "./includes/Form"
+import { Input, FormBtn } from "./includes/Form"
 
 class Signup extends Component {
   state = {
@@ -10,6 +10,9 @@ class Signup extends Component {
     password: ""
   }
 
+  componentDidMount() {
+  }
+
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
@@ -17,45 +20,47 @@ class Signup extends Component {
     });
   };
 
-handleFormSubmit = event => {
-  event.preventDefault();
-  if (this.state.username && this.state.email && this.state.password) {
-    API.saveUser({
-      username: this.state.username,
-      email: this.state.email,
-      password: this.state.password
-    })
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.username && this.state.email && this.state.password) {
+      API.saveUser({
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password
+      })
+        .then(res => {
+          console.log("User saved by API");
+          console.log(res);
+        });
+    }
   }
-}
 
 
-  render () {
-  return (
-    <form className="border bg-light p-3 rounded">
-      <Input
-      value = {this.state.username}
-      onChange = {this.handleInputChange}
-      name = "username"
-      placeholder = "Username (Required)"
-      />
-       <Input
-      value = {this.state.email}
-      onChange = {this.handleInputChange}
-      name = "email"
-      placeholder = "Email (Required)"
-      />
-       <Input
-      value = {this.state.password}
-      onChange = {this.handleInputChange}
-      name = "password"
-      placeholder = "Password (Required)"
-      />
-      <FormBtn onClick = {this.handleFormSubmit}>Sign Up</FormBtn>
-    </form>
-  );
-}
+  render() {
+    return (
+      <form className="border bg-light p-3 rounded">
+        <Input
+          value={this.state.username}
+          onChange={this.handleInputChange}
+          name="username"
+          placeholder="Username (Required)"
+        />
+        <Input
+          value={this.state.email}
+          onChange={this.handleInputChange}
+          name="email"
+          placeholder="Email (Required)"
+        />
+        <Input
+          value={this.state.password}
+          onChange={this.handleInputChange}
+          name="password"
+          placeholder="Password (Required)"
+        />
+        <FormBtn onClick={this.handleFormSubmit}>Sign Up</FormBtn>
+      </form>
+    );
+  }
 }
 
 export default Signup;
