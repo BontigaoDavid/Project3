b5556dd38dc511f0bd4fdaca177db12d8f1606c0
J Tsai <jtsai972@gmail.com>
2019-11-21 20:28:01 -0600

    adding buttons to lesson

--- client/src/components/lesson/css/box-model/BoxModel.js
+++ client/src/components/lesson/css/box-model/BoxModel.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Box from "./Box";
 import BoxLegend from "./BoxLegend";
+import BoxButtons from "./BoxButtons";
 
 function BoxModel() {
   return (
     <div className="row">
-      <div className="col-12 col-md-3 col-lg-5"></div>
-      <div className="col-12 col-md-9 col-lg-7">
+      <div className="col-12 col-md-5 col-lg-6">
+        <BoxButtons />
+      </div>
+      <div className="col-12 col-md-7 col-lg-6">
         <div className="card">
           <div className="card-header d-flex justify-content-center">
             <BoxLegend />
