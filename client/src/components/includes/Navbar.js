6be96d1e00793d6f176c033cce05ab04b0b0eaa7
J Tsai <jtsai972@gmail.com>
2019-11-25 09:52:08 -0600

    messing with the navbar

---
+++ client/src/components/includes/Navbar.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark navbar-expand-lg bg-secondary text-right">
+        <div className="container">
+          {/* <a className="navbar-brand" href="/">
+            Navbar w/ text
+          </a> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className={
+                    window.location.pathname === "/"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  <i class="fas fa-home"></i> Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/lesson"
+                  className={
+                    window.location.pathname === "/lesson"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Lesson Page
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/about"
+                  className={
+                    window.location.pathname === "/about"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  About Page
+                </Link>
+              </li>
+              {/* if(sessionStorage.getItem("isAuthorized") {
+                <li className="nav-item">
+                  <Link
+                    to="/user"
+                    className={
+                      window.location.pathname === "/user"
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                  >
+                    Profile Page
+                  </Link>
+                </li>
+              } */}
+            </ul>
+            {/* <span className="navbar-text">
+              Navbar text with an inline element
+            </span> */}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
