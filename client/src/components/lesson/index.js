134e09468c5858acf710edcea63d09f8be70493b
J Tsai <jtsai972@gmail.com>
2019-12-03 11:31:59 -0600

    updating the toc

--- client/src/components/lesson/index.js
+++ client/src/components/lesson/index.js
@@ -9,7 +9,7 @@ function tableOfContents() {
             <div className="row">
                 <div className="col-12 col-md-4 d-flex">
                     <div className="card toc justify-content-stretch w-100">
-                        <div className="card-header">CSS</div>
+                        <div className="card-header"><i className="fab fa-css3"></i> CSS</div>
                         <div className="card-body py-2">
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item">
@@ -21,7 +21,7 @@ function tableOfContents() {
                 </div>
                 <div className="col-12 col-md-4 d-flex">
                     <div className="card toc justify-content-stretch w-100">
-                        <div className="card-header">ES6</div>
+                        <div className="card-header"><i className="fab fa-code"></i> ES6</div>
                         <div className="card-body py-2">
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item">
@@ -36,7 +36,7 @@ function tableOfContents() {
                 </div>
                 <div className="col-12 col-md-4 d-flex">
                     <div className="card toc justify-content-stretch w-100">
-                        <div className="card-header">Algorithms</div>
+                        <div className="card-header"><i className="fab fa-superscript"></i> Algorithms</div>
                         <div className="card-body py-2">
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item">
