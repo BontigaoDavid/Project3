f0e83d40b1f1c8938e61a7319139a6b548e3c07a
J Tsai <jtsai972@gmail.com>
2019-12-04 09:52:58 -0600

    bolding title and adjusting subtitle size

--- client/src/pages/includes/Header.js
+++ client/src/pages/includes/Header.js
@@ -4,8 +4,8 @@ function Header () {
   return (
     <header className="py-3 header">
       <div className="container">
-        <h1 id = "title" className="lead mb-0" style={{fontSize: "3em"}}>Simply Conceptual</h1>
-        <p id="subtitle" className="lead mb-0" style={{fontSize: "2em"}}>Back to the Basics with Web Development</p>
+        <h1 id = "title" className="lead mb-0" style={{fontSize: "3em", fontWeight: "500"}}>Simply Conceptual</h1>
+        <p id="subtitle" className="lead mb-0" style={{fontSize: "1.75em"}}>Back to the Basics with Web Development</p>
       </div>
     </header>
   );
