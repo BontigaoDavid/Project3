8fb983401860c0a39590b8a0176a981c79bbeec5
J Tsai <jtsai972@gmail.com>
2019-12-04 09:04:01 -0600

    added some section styling

--- client/src/components/lesson/css/box-model/Lesson.js
+++ client/src/components/lesson/css/box-model/Lesson.js
@@ -40,7 +40,7 @@ class BoxModel extends React.Component {
       padding: "p-4",
       text: ["border: 0.4em solid #FFD500", "margin: 1.5rem", "padding: 1.5rem"]
     });
-  }
+  };
 
   handleClick = stateVal => {
     console.log(stateVal);
@@ -51,43 +51,49 @@ class BoxModel extends React.Component {
   render() {
     // console.log(this.state);
     return (
-      <div className="container">
-        <h2>CSS Box Model</h2>
+      <div>
+        <div className="container">
+          <h2>CSS Box Model</h2>
+        </div>
+
         <Explanation />
-        <h3>Interactive CSS Box Model</h3>
-        <div className="row">
-          <div className="col-12 col-lg-5">
-            <BoxButtons style={this.state} handler={this.handleClick} />
-          </div>
-          <div className="col-12 col-lg-7">
-            <div className="card">
-              <div className="card-header">
-                <BoxLegend />
+        <div className="container">
+          <h3>Interactive CSS Box Model</h3>
+          <div className="row">
+            <div className="col-12 col-lg-5">
+              <BoxButtons style={this.state} handler={this.handleClick} />
+            </div>
+            <div className="col-12 col-lg-7">
+              <div className="card">
+                <div className="card-header">
+                  <BoxLegend />
+                </div>
+                <div className="card-body d-flex align-items-center justify-content-center">
+                  <Box
+                    id="variable"
+                    margin={this.state.margin}
+                    padding={this.state.padding}
+                    border={this.state.border}
+                  />
+                  <Box margin="m-4" padding="p-4" border="p-1" />
+                </div>
+                <div className="card-footer d-flex justify-content-center">
+                  <code>
+                    {this.state.text[0]}; <br />
+                    {this.state.text[1]}; <br />
+                    {this.state.text[2]};
+                  </code>
+                </div>
               </div>
-              <div className="card-body d-flex align-items-center justify-content-center">
-                <Box
-                  id="variable"
-                  margin={this.state.margin}
-                  padding={this.state.padding}
-                  border={this.state.border}
-                />
-                <Box margin="m-4" padding="p-4" border="p-1" />
-              </div>
-              <div className="card-footer d-flex justify-content-center">
-                <code>
-                  {this.state.text[0]}; <br />
-                  {this.state.text[1]}; <br />
-                  {this.state.text[2]};
-                </code>
-              </div>
-            </div>{/* End Card */}
-            <button
-              type="button"
-              onClick={this.handleReset}
-              className={`btn btn-secondary btn-block my-4`}
-            >
-              Reset Box Model
-            </button>
+              {/* End Card */}
+              <button
+                type="button"
+                onClick={this.handleReset}
+                className={`btn btn-secondary btn-block my-4`}
+              >
+                Reset Box Model
+              </button>
+            </div>
           </div>
         </div>
       </div>
