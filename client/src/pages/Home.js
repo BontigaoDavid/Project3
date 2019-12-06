7f070e8a485d0ecb3f5c58010341b1158c5da035
J Tsai <jtsai972@gmail.com>
2019-12-06 17:06:31 -0600

    making the signup disappear when loged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,8 +9,11 @@ function Home() {
       <div className="row">
         <div className="col-12 col-md-6 col-lg-7">
           <h1 className="welcomeTitle display-3">Welcome to our learning platform!</h1>
-          <p className="welcomeSubtitle lead">
+          <p className={sessionStorage.getItem("isAuthorized") ? "d-none" : "welcomeSubtitle lead"}>
             If you want to keep track of your progress, it's easy, just sign up!
+          </p>
+          <p className={sessionStorage.getItem("isAuthorized") ? "welcomeSubtitle lead" : "d-none"}>
+            Thank you so much for joining us!
           </p>
         </div>
         <Usertab />
